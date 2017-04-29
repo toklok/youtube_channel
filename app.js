c042e99e34399274a45aa3d03fd6dc43aa4ecceb
Joe Curtis <joecurtis@joes-mbp.fios-router.home>
2017-04-28 23:35:55 -0400

    Update HTML, CSS for site

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ $(document).ready(function() {
   $('#slick').slick({
     mobileFirst: true,
     swipe: true,
+    nextArrow: '<div class="carousel-button"></div>',
+    prevArrow: '<div class="carousel-button"></div>',
     touchMove: true
   });
 });
